82791333ac205028c25e70620503e3b8ad1236fd
Gurukiran <gurukiranhosur615@gmail.com>
2026-03-22 00:03:31 +0530

    Fix API routes

--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -11,7 +11,7 @@ function Signup() {
   const handleSignup = async () => {
     try {
       await axios.post(
-        "https://zerodha-backend-j6zw.onrender.com/api/auth/signup",
+        "https://zerodha-backend-j6zw.onrender.com/register",
         data,
       );
       alert("Signup successful");
